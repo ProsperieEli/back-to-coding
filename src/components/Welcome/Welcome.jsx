a3bada6eac9eb2ded369bb2c97a6e8ea63b655f6
ElijahP <ElijahProsperie@gmail.com>
2023-04-18 11:27:13 -0700

    worked in Welcome.jsx. Added 3 photos with a transition

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -4,10 +4,20 @@
     */
 
     import React from 'react'
-    
+import welcometext from './IntroText'
+import logo from '../../logo.svg'
+import './Welcome.css'    
     function Welcome() {
       return (
-        <div>Welcome</div>
+        <>
+        <div>{welcometext}</div>
+      <div className='intro'>
+      <img src="elden-ring-cover-art.jpg" alt="Elden Ring" />
+      <img src="God-of-War.jpg" alt="God of War" />
+      <img src="Resident_Evil_4_remake_cover_art.jpg" alt="Resident Evil 4 Remake" />
+    </div>
+    
+        </>
       )
     }
     
