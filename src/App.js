6669b94e165e925bb1f89baf73244c77058e1c19
ElijahP <ElijahProsperie@gmail.com>
2023-04-18 11:02:11 -0700

    file format complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Home from './components/Home/Home'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Let's get started!
-      </header>
-    </div>
+    <>
+    <Home />
+    </>
   );
 }
 
---
+++ src/components/Welcome/Welcome.jsx
@@ -0,0 +1,14 @@
+/*
+    Here, we do an intro to the app, brief explanation, and styling to wow the user.
+    A button that takes the user to the next page and a form to take down their username
+    */
+
+    import React from 'react'
+    
+    function Welcome() {
+      return (
+        <div>Welcome</div>
+      )
+    }
+    
+    export default Welcome
