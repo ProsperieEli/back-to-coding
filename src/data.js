9cee7029c0e0d164f8672af016d195daadeecced
ElijahP <ElijahProsperie@gmail.com>
2023-04-18 10:11:03 -0700

    created a dummy data in data.js

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const gameCatelog = [
+    {
+        name: 'The Legend of Zelda: Breath of the Wild',
+        price: 59.99,
+        description: 'The Legend of Zelda: Breath of the Wild is an action-adventure game developed and published by Nintendo for the Nintendo Switch and Wii U video game consoles. The 19th installment in The Legend of Zelda series, it was released worldwide for the Switch and Wii U in March 2017. The game is set in an open world environment, and players control Link, who awakens from a 100-year slumber to a mysterious voice that guides him to defeat Calamity Ganon, a corrupted version of the kingdom of Hyrule. The game is played from a third-person perspective and its world is navigated on foot or by riding horses, camels, or other animals. Players use a variety of weapons and items to fight enemies, solve puzzles, and interact with characters and the environment. The game features a physics engine that allows objects to be interacted with in a variety of ways, such as using a magnet to move metal objects. The game also features a day and night cycle, and weather systems that affect the environment and gameplay. The game received universal acclaim from critics, who praised its open world design, physics engine, and visuals. It won numerous Game of the Year awards, and was the best-selling game of 2017. It was also the best-selling game of the 2010s decade, selling over 17 million copies worldwide. A sequel, The Legend of Zelda: Breath of the Wild 2, is in development for the Nintendo Switch.',
+        image: 'https://upload.wikimedia.org/wikipedia/en/0/0d/The_Legend_of_Zelda_Breath_of_the_Wild.jpg',
+        category: 'Action',
+        rating: 5,
+        id: 1,
+        release_year: 2017,
+        developer: 'Nintendo',
+        publisher: 'Nintendo',
+    },
+    {
+        name: 'Sonic Frontiers',
+        price: 59.99,
+        description: 'Sonic is back and better than ever. Fight collasol beasts and save the world from the evil Dr. Eggman.',
+        image: 'https://bloximages.chicago2.vip.townnews.com/tiftongazette.com/content/tncms/assets/v3/editorial/c/1c/c1cdc652-7fed-11ed-b8e6-ebb0b8882f93/63a0e7cc9aee7.image.jpg?resize=800%2C800',
+        category: 'Action',
+        rating: 4,
+        id: 2,
+        release_year: 2021,
+        developer: 'Sega',
+        publisher: 'Sega',
+    },
+    {
+        name: 'Resident Evil 4',
+        price: 59.99,
+        description: 'Resident Evil 4 is a 2005 survival horror video game developed and published by Capcom. It is the sixth installment in the Resident Evil series and the direct sequel to Resident Evil 3: Nemesis. The game was originally released for the GameCube in 2005, and has since been ported to other platforms, including the Wii, PlayStation 2, PlayStation 3, Xbox, Xbox 360, and Microsoft Windows. The game follows U.S. government agent Leon S. Kennedy, who is sent on a mission to rescue the President\'s daughter Ashley Graham from a mysterious cult. The game is played from a third-person perspective, and the player uses firearms and melee weapons to fight enemies. The game features a new over-the-shoulder camera, which allows the player to aim and shoot while moving. The game also features a new dodge mechanic, which allows the player to evade enemy attacks. The game received universal acclaim from critics, who praised its gameplay, graphics, and story. It has sold over 7 million copies worldwide, and is considered one of the best games of all time. A remake of the game, Resident Evil 2, was released in 2019. A sequel, Resident Evil 5, was released in 2009. A third-person shooter spin-off, Resident Evil: Operation Raccoon City, was released in 2012. A fourth installment, Resident Evil 6, was released in 2012. A fifth installment, Resident Evil 7: Biohazard, was released in 2017. A sixth installment, Resident Evil 8: Village, was released in 2021.',
+        image: 'https://i0.wp.com/news.xbox.com/en-us/wp-content/uploads/sites/2/2023/03/RE4-4da975dc612a299677bd.jpg?resize=720%2C675&ssl=1',
+        category: 'Action',
+        rating: 5,
+        id: 3,
+        release_year: 2023,
+        developer: 'Capcom',
+        publisher: 'Capcom',
+    }
+];
+
+export default gameCatelog;
