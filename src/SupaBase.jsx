8de7d6b1c84d4fd9d7d489d8ba02624f42a0c3f1
ElijahP <ElijahProsperie@gmail.com>
2023-04-24 11:42:49 -0700

    fetching from supaBase database in console

--- src/SupaBase.jsx
+++ src/SupaBase.jsx
@@ -1,1 +1,21 @@
 /* Grab data from SupaBase here*/
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://fiprssgmvhvbvduuybrv.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpcHJzc2dtdmh2YnZkdXV5YnJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIwMTM2NzAsImV4cCI6MTk5NzU4OTY3MH0.xFL181rObIi8P8FzHhmeyvvM6WC2PAo6paabBw0vaMA';
+const supabase = createClient(supabaseUrl, supabaseKey);
+console.log(supabase)
+
+async function getSupaBaseData() {
+    const { data } = await supabase
+    .from('catelog')
+    .select();
+    if(data) {
+        console.log('about to return data')
+        console.log(data);
+    }
+      return data;
+  }
+
+export default getSupaBaseData;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,13 +2,29 @@
     Here is where the list of all games in the database will be displayed. User can click on one to either favorite it or view more info about it.
     */
 
-    import React from 'react'
+    import React, { useEffect } from 'react'
     import gameCatelog from '../../data'
     import './Home.css'
 import { Link, redirect } from 'react-router-dom'
+import { useState } from 'react'
+import getSupaBaseData from '../../SupaBase'
 
     function Home() {
-      
+      const [games, setGames] = useState([])
+
+      useEffect(() => {
+        setGames(getSupaBaseData)
+        console.log(games, 'test')
+      }, [])
+
+      const handleClickLike = (e) => {
+        if(games.favorited === true) {
+          return games.favorited = true
+        }
+        else {
+          return games.favorited = false
+        }
+      }
       const handleClick = (e) => {
         console.log(`game.id: ${e.target.id}`)
         redirect('/game')
@@ -16,17 +32,17 @@ import { Link, redirect } from 'react-router-dom'
 
       return (
         <div>
-          {gameCatelog.map((game) =>{
-            return (
-              <div className='games'>
-                <img src={game.image} alt={game.title} />
-                <h1>{game.title}</h1>
-                <p>{game.description}</p>
-                <button>Favorite?</button>
-                <Link to={`/game/${game.id}`} onClick={handleClick}>View More</Link>
-              </div>
+                <h1>Game Catelog</h1>
+                <ul>
+                  {games.map((game) => (
+                    <li key={game.id}>
+                      {game.name}, {game.genre}, {game.rating}
+                      <button id={game.id} onClick={handleClickLike}>Favorite?</button>
+                      <Link to={`/game/${game.id}`} onClick={handleClick}>View More</Link>                    
+                    </li>
+                  ))}
+                  </ul>
             )
-          })}
         </div>
       )
     }
